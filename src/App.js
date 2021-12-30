31fab7183ae34f1ec4f2be8d7ab616ba348ca53f
leohersant <leohersant@gmail.com>
2021-12-30 15:42:33 -0500

    Adding Variables and functions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const name = "Leo";
+
+  const handleNameChange = () => {
+    const names = ['Leo','Junior','Daniel','Katy','Shelly','Ashley','Rony','Ise','Mario'];
+    const intx = Math.floor(Math.random() * 7);
+    return names[intx];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see changes.
         </p>
+        <p>Hello {name}!</p>
+        <p>Your firend is {handleNameChange()}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
