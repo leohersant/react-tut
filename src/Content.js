0b33e7c6fc9efac19512c4422fe95588c6b4b0aa
leohersant <leohersant@gmail.com>
2021-12-31 14:42:01 -0500

    Click events

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,29 @@ const Content = () => {
         return names[int];
     }
 
+    const handleClick = () => {
+        console.log('You click');
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target);
+    }
+
     return (
         <main>
             <p>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click me</button>
+            
+            <button onClick={ () => handleClick2('Leo')}>Click me 2</button>
+
+            <button onClick={ (e) => handleClick3(e)}>Click me 3</button>
+            
         </main>
     )
 }
