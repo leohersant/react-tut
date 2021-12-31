5fa58d6f1f22887b43814ecf2d85bce1b453c00f
leohersant <leohersant@gmail.com>
2021-12-31 15:27:31 -0500

    state count

--- src/Content.js
+++ src/Content.js
@@ -5,12 +5,17 @@ const Content = () => {
     // Getter and Setter (State). Daniel is default value.
     // This is an array of variables. getName = 'Daniel' by default
     const [getName, setName] = useState('Daniel');
+    const [getCount, setCount] = useState(0);
 
 
     const handleNameChange = () => {
         const names = ['Leon', 'Daniel', 'Junior'];
         const int = Math.floor(Math.random() * 3);
         return setName(names[int]);
+    }
+
+    const handleCountChange = () => {      
+        return setCount(getCount + 1);
     }
 
     const handleClick = () => {
@@ -28,14 +33,19 @@ const Content = () => {
     return (
         <main>
             <p onDoubleClick={handleClick}>
-                Hello {getName}!
+                Hello {getName}! 
             </p>
 
+            <p>Count = {getCount}</p>
+
             <button onClick={handleNameChange}>Change name</button>
+
+            <button onClick={handleCountChange}>Count</button>
 
             <button onClick={() => handleClick2('Leo')}>Click me 2</button>
 
             <button onClick={(e) => handleClick3(e)}>Click me 3</button>
+            
 
         </main>
     )
