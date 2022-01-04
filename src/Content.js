bbb0ace30dab3426888228a0e4b995aec3a148a7
leohersant <leohersant@gmail.com>
2022-01-04 16:06:55 -0500

    handle check and delete (chap 7)

--- src/Content.js
+++ src/Content.js
@@ -24,21 +24,44 @@ const Content = () => {
     );
 
     const handleCheck = (id) => {
-        console.log(`key: ${id}`);
+        //console.log(`key: ${id}`);
+        const listItems = items.map((item) => item.id === id ? {
+            ...item,
+            checked: !item.checked
+        } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        // console.log(`key: ${id}`);
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     }
 
     return (
         <main>
-            <ul>
-                {items.map((item) => (<li className="item" key={item.id}>
-                    <input
-                        type="checkbox"
-                        checked={item.checked}
-                        onChange={() => handleCheck(item.id)} />
-                    <label>{item.item}</label>
-                    <FaTrashAlt role="button" tabIndex="0" />
-                </li>))}
-            </ul>
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (<li className="item" key={item.id}>
+                        <input
+                            type="checkbox"
+                            checked={item.checked}
+                            onChange={() => handleCheck(item.id)} />
+                        <label
+                            style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                            onDoubleClick={() => handleCheck(item.id)}
+                        >{item.item}</label>
+                        <FaTrashAlt
+                            role="button"
+                            tabIndex="0"
+                            onClick={() => handleDelete(item.id)} />
+                    </li>))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+            )}
         </main>
     )
 }
